da0228751cbfc348fe43950e8e3c40c235c95545
athira <athira@qburst.com>
2017-02-17 17:20:49 +0530

    Added images and json

--- js/design.js
+++ js/design.js
@@ -2,9 +2,15 @@ $(document).ready(function() {
     $.getJSON("json/design.json", function(result) {
         var designArr;
         $.each(result, function(i, field) {
-            field[i].push(designArr);
+            designArr = field;
         });
-        for (i = 0; i < designArr.length; i++)
-            $(".design-wrap").append('<a>' + designArr[i].title + '</a>');
+        for (i = 0; i < designArr.length; i++) {
+            var array = $.map(designArr[i], function(value, index) {
+                return value;
+            });
+            console.log(array);
+            for (j = 0; j < array.length; j++)
+                $(".design-wrap").append('<a>' + array[j] + '</a>');
+        }
     });
 });
