1f2f9603573852ab23abaadc812e46d481aecfb8
athira <athira@qburst.com>
2017-04-05 13:35:33 +0530

    Code cleaning

--- js/design.js
+++ js/design.js
@@ -7,16 +7,18 @@ $(document).ready(function() {
             });
             var length = designArr.length;
             for (i = 0; i < length; i++) {
-                var title = designArr[i].title;
+                var title = designArr[i].title,
+                    desginUrl = designArr[i];
                 $(".design-section").append('<div class="design-block"><h2>' + title + '</h2></div>');
-                $.each(designArr[i], function(index, value) {
+                $.each(desginUrl, function(index, value) {
                     if (index != 'title') {
-                        var val = value.split('.').pop();
+                        var val = value.split('.').pop(),
+                            lastElement = $(".design-block").last();
                         if (val == "html")
-                            $(".design-block").last().append('<p><a href="/DesignHostingApp' + value + '"  target="_blank">' + index + '</a></p>');
+                            lastElement.append('<p><a href="/DesignHostingApp' + value + '"  target="_blank">' + index + '</a></p>');
                         else
-                            $(".design-block").last().append('<p><a href="/DesignHostingApp/design.html" target="_blank" data-url=' + value + '>' + index + '</a></p>');
-                        $(".design-block a").on('click auxclick', function(e) {
+                            lastElement.append('<p><a href="/DesignHostingApp/design.html" target="_blank" data-url=' + value + '>' + index + '</a></p>');
+                        $(".design-block a").on("click auxclick", function(e) {
                             var url = $(this).attr("data-url");
                             localStorage.setItem('desgin', url);
                         });
